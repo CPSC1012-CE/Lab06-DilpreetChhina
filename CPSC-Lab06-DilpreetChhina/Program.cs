41088f13464c16144fd4d487a5e844c69bf8ace8
Dilpreet singh chhina <90094641+Dilpreets21@users.noreply.github.com>
2022-12-03 17:30:14 -0700

    Added Project

---
+++ CPSC-Lab06-DilpreetChhina/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CPSC_Lab06_DilpreetChhina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Purpose: Creating simple class with properties, constructor, and a instance-level method  
+            //Input: int minutes, int km
+            //Output: double fair
+            //Written by: Dilpreet Chhina
+            //Written for: Carlos Estoy
+            //Section: E01
+            //Last Modified Date: 2022-12-03
+            int minutes = 0, km = 0;
+            double fair = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Welcome to the Taxi Fare Calculator");
+                    Console.WriteLine("Enter the time, in whole minutes. te trip took (min 10 minutes)");
+
+                    while (!int.TryParse(Console.ReadLine(), out minutes))
+                    {
+                        Console.WriteLine("Wrong input, try again...");
+                        Console.WriteLine("Enter the time, in whole minutes. te trip took (min 10 minutes)");
+                    }
+
+                    Console.WriteLine("Enter the distance, in whole kilometer, the trip was (min 2 km)");
+
+                    while (!int.TryParse(Console.ReadLine(), out km))
+                    {
+                        Console.WriteLine("Wrong input, try again...");
+                        Console.WriteLine("Enter the distance, in whole kilometer, the trip was (min 2 km)");
+                    }
+
+                    TaxiFare taxi = new TaxiFare(minutes, km);
+                    fair = taxi.CalculateFair(minutes, km);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(1);    //Exiting the program if there is an exception
+                }
+            }
+
+            while (minutes < 10 || km < 2);
+
+            Console.WriteLine("The fare for the pssenger is {0:C}", fair);
+        }
+    }
+}
---
+++ CPSC-Lab06-DilpreetChhina/TaxiFare.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSC_Lab06_DilpreetChhina
+{
+    internal class TaxiFare
+    {
+        public int _minutes { get; set; }
+        public int _kmTravelled { get; set; }
+
+        public double Fair { get; set; }
+
+        public int Minutes
+        {
+            get { return _minutes; }
+            set
+            {
+                if (value < 10)
+                {
+                    throw new Exception("Invalid trip minutes, Minimum minutes are 10");
+                }
+                _minutes = value;
+            }
+        }
+        public int KmTravelled
+        {
+            get { return _kmTravelled; }
+            set
+            {
+                if (value < 2)
+                {
+                    throw new Exception("Invalid trip kilimeters, Minimum kilometers are 2");
+                }
+                _kmTravelled = value;
+            }
+        }
+        public TaxiFare(int minutes, int km)
+        {
+            Minutes = minutes;
+            KmTravelled = km;
+            CalculateFair(minutes, km);
+
+        }
+
+        public double CalculateFair(int minutes, int km)
+        {
+            Fair = (minutes * 0.25 + km * 1.25) * 1.10;
+            return Fair;
+        }
+
+
+    }
+}
